0f717ca074344d02c16c52f6c213f6acfd6befe7
Stella Tran <stellatranthi@hotmail.com>
2022-11-06 00:41:30 -0700

    Created rough register/login with firebase auth

--- app/src/main/java/ca/sfu/minerva/MainActivity.kt
+++ app/src/main/java/ca/sfu/minerva/MainActivity.kt
@@ -1,11 +1,74 @@
 package ca.sfu.minerva
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var emailET: EditText
+    private lateinit var passwordET: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnRegister: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        emailET = findViewById(R.id.email)
+        passwordET = findViewById(R.id.password)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnRegister = findViewById(R.id.btnRegister)
+
+        auth = Firebase.auth
+
+        btnLogin.setOnClickListener {
+            login()
+        }
+
+        btnRegister.setOnClickListener {
+            register()
+        }
     }
+
+    private fun login() {
+        val email = emailET.text.toString()
+        val pass = passwordET.text.toString()
+        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                val intent = Intent(this, MapsActivity::class.java)
+                startActivity(intent)
+            } else
+                Toast.makeText(this, "Invalid Credentials", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun register() {
+        val email = emailET.text.toString()
+        val pass = passwordET.text.toString()
+
+        if (email.isBlank() || pass.isBlank()) {
+            Toast.makeText(this, "Email and/or Password is blank", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Account Created", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MapsActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Registration Failed. Please try again later.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+
 }
