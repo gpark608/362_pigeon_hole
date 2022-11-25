319410e1cb1946d01950c04aaa5fad019eaf8744
Linden Kyaw <kyawlinden@gmail.com>
2022-11-24 18:42:57 -0800

    Add functions to convert LatLng lists to String and vice versa

--- app/src/main/java/ca/sfu/minerva/util/Helper.kt
+++ app/src/main/java/ca/sfu/minerva/util/Helper.kt
@@ -1,5 +1,11 @@
 package ca.sfu.minerva.util
 
+import com.google.android.gms.maps.model.LatLng
+import com.google.common.reflect.TypeToken
+import com.google.gson.Gson
+import java.lang.reflect.Type
+import java.util.ArrayList
+
 object Helper {
     private fun getTimeInSeconds(start: Long, end: Long): Double {
         return ((end - start) / 1000).toDouble()
@@ -16,5 +22,17 @@ object Helper {
 
     fun metersToMiles(meters: Float): Float {
         return (meters / 1609.344).toFloat()
+    }
+
+    fun jsonToLatLng(json: String): ArrayList<LatLng> {
+        val gson = Gson()
+        val listType: Type = object : TypeToken<ArrayList<LatLng>>() {}.type
+        return gson.fromJson(json, listType)
+    }
+
+    fun latLngToString(list: ArrayList<LatLng>): String {
+        val gson = Gson()
+        val listType: Type = object : TypeToken<ArrayList<LatLng>>() {}.type
+        return gson.toJson(list, listType)
     }
 }
