182877887c48d1b2450c04aeea775b5e50896f73
Linden Kyaw <kyawlinden@gmail.com>
2022-12-02 16:58:51 -0800

    Add toolbar and function to setup toolbar programmatically

--- app/src/main/java/ca/sfu/minerva/ui/profile/EditProfileActivity.kt
+++ app/src/main/java/ca/sfu/minerva/ui/profile/EditProfileActivity.kt
@@ -1,12 +1,30 @@
 package ca.sfu.minerva.ui.profile
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import ca.sfu.minerva.R
+import com.google.android.material.appbar.MaterialToolbar
 
 class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+
+        setupToolBar()
+    }
+
+    private fun setupToolBar() {
+        val toolbar: MaterialToolbar = findViewById(R.id.materialToolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
