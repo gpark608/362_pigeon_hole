1a0325f458312d55e54f66860c967fab8b0e6386
Linden Kyaw <kyawlinden@gmail.com>
2022-12-05 15:44:57 -0800

    Setup toolbar and add default values for list preference

--- app/src/main/java/ca/sfu/minerva/SettingsActivity.kt
+++ app/src/main/java/ca/sfu/minerva/SettingsActivity.kt
@@ -2,15 +2,21 @@ package ca.sfu.minerva
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
+    private lateinit var toolbar: MaterialToolbar
 
     companion object {
         const val PROJECT_529_KEY = "project529"
         const val CHANGE_BACKGROUND_KEY = "background"
+        const val MEASURE_KEY = "units_of_measure"
+        const val WEIGHT_KEY = "units_of_weight"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,15 +28,39 @@ class SettingsActivity : AppCompatActivity() {
                 .replace(R.id.settings, SettingsFragment())
                 .commit()
         }
+
+        // load toolbar before setup
+        toolbar = this.findViewById(R.id.toolbar_settings)
+        setupToolBar()
+    }
+
+    private fun setupToolBar() {
+        setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
+        private lateinit var measureUnitPreference: ListPreference
+        private lateinit var weightUnitPreference: ListPreference
         private lateinit var btnProject529: Preference
         private lateinit var btnChangeBackground: Preference
 
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            measureUnitPreference = findPreference(MEASURE_KEY)!!
+            measureUnitPreference.setValueIndex(0);
+
+            weightUnitPreference = findPreference(WEIGHT_KEY)!!
+            weightUnitPreference.setValueIndex(0);
 
             btnProject529 = findPreference(PROJECT_529_KEY)!!
             btnProject529.setOnPreferenceClickListener {
