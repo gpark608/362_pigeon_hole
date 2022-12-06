85f2ebdad8b1f27d1a8690731792849bf897b667
BG30 <50424545+BG30@users.noreply.github.com>
2022-12-06 00:07:08 -0800

    improved stability of service, added topspeed to bikeusage

--- app/src/main/java/ca/sfu/minerva/ui/map/MapFragment.kt
+++ app/src/main/java/ca/sfu/minerva/ui/map/MapFragment.kt
@@ -154,6 +154,9 @@ class MapFragment : Fragment(), OnMapReadyCallback, LocationListener, GoogleMap.
         buttonTracking = root.findViewById(R.id.buttonTracking)
         trackingViewModel = ViewModelProvider(this)[TrackingViewModel::class.java]
         trackingServiceIntent = Intent(requireActivity(), TrackingService::class.java)
+        if(viewModel.bikeTrackingToggle)
+            buttonTracking.text = "Stop Tracking"
+
 
         return root
     }
@@ -361,14 +364,20 @@ class MapFragment : Fragment(), OnMapReadyCallback, LocationListener, GoogleMap.
 
         }
 
+
+        if(viewModel.bikeTrackingToggle)
+            buttonTracking.text = "Stop Tracking"
+
         buttonTracking.setOnClickListener {
             if(!viewModel.bikeTrackingToggle){
                 startTrackingService()
+                Log.d("tracking", "Tracking status: started")
                 buttonTracking.text = "Stop Tracking"
             }
             else {
                 // Turning off the service
                 stopTrackingService()
+                Log.d("tracking", "Tracking status: stopped")
                 buttonTracking.text = "Start Tracking"
             }
 
@@ -450,12 +459,14 @@ class MapFragment : Fragment(), OnMapReadyCallback, LocationListener, GoogleMap.
     }
 
     private fun startTrackingService(){
-        ContextCompat.startForegroundService(requireActivity(), trackingServiceIntent)
-//        requireActivity().application.startForegroundService(trackingServiceIntent)
+        requireActivity().applicationContext.startForegroundService(trackingServiceIntent)
+        //ContextCompat.startForegroundService(requireActivity().applicationContext, trackingServiceIntent)
         requireActivity().applicationContext.bindService(trackingServiceIntent, trackingViewModel, Context.BIND_AUTO_CREATE)
 
         trackingViewModel.mapBundle.observe(viewLifecycleOwner){
             latestTrackingBundle = it
+            Log.d("bike usage detail", "new bundle received")
+            Log.d("bike usage data", "$latestTrackingBundle")
         }
 
     }
@@ -464,17 +475,24 @@ class MapFragment : Fragment(), OnMapReadyCallback, LocationListener, GoogleMap.
         requireActivity().applicationContext.unbindService(trackingViewModel)
         requireActivity().applicationContext.stopService(trackingServiceIntent)
 
-        if(latestTrackingBundle != null) {
 
+
+        if(latestTrackingBundle != null) {
             CoroutineScope(IO).launch {
                 // save the current usage into db
+                val temp = latestTrackingBundle
+                Log.d("bike usage data", "using the temp: $temp")
                 var bikeUsage = BikeUsage()
-                bikeUsage.climb = latestTrackingBundle!!.getFloat("currentAltitude").toDouble()
-                bikeUsage.date = latestTrackingBundle!!.getFloat("date").toString()
-                bikeUsage.duration = latestTrackingBundle!!.getDouble("elapsedTime").toString()
-                bikeUsage.distance = latestTrackingBundle!!.getDouble("elapsedDistance")
-                bikeUsage.avgSpeed = latestTrackingBundle!!.getDouble("avgSpeed")
-                bikeUsage.time = latestTrackingBundle!!.getDouble("startTime").toString()
+                bikeUsage.climb = temp!!.getFloat("currentAltitude").toDouble()
+                bikeUsage.date = temp!!.getFloat("date").toString()
+                bikeUsage.duration = temp!!.getDouble("elapsedTime").toString()
+                bikeUsage.distance = temp!!.getDouble("elapsedDistance")
+                bikeUsage.avgSpeed = temp!!.getDouble("avgSpeed")
+                bikeUsage.time = temp!!.getDouble("startTime").toString()
+                bikeUsage.speed = temp!!.getDouble("speed")
+
+                Log.d("bike usage detail", "$bikeUsage")
+
 
                 var tempArr = ArrayList<LatLng>()
 
@@ -488,6 +506,7 @@ class MapFragment : Fragment(), OnMapReadyCallback, LocationListener, GoogleMap.
                 bikeUsage.locationList = tempArr
 
                 viewModel.insertBikeUsage(bikeUsage)
+                latestTrackingBundle!!.clear()
             }
 
             Toast.makeText(
@@ -495,7 +514,6 @@ class MapFragment : Fragment(), OnMapReadyCallback, LocationListener, GoogleMap.
                 "Saved biking event",
                 Toast.LENGTH_SHORT
             ).show()
-            latestTrackingBundle!!.clear()
         }
     }
 
@@ -572,7 +590,6 @@ class MapFragment : Fragment(), OnMapReadyCallback, LocationListener, GoogleMap.
 
         buttonList.visibility = View.VISIBLE
 
-        Log.d("poilist", "${poiDataList.size}")
         for(favourite in poiDataList){
             poiMarkers.add(mMap.addMarker(poiMarkerOption.title(favourite.name)
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
--- app/src/main/java/ca/sfu/minerva/util/TrackingService.kt
+++ app/src/main/java/ca/sfu/minerva/util/TrackingService.kt
@@ -12,8 +12,8 @@ import android.location.LocationListener
 import android.location.LocationManager
 import android.os.*
 import androidx.core.app.NotificationCompat
+import ca.sfu.minerva.CoreActivity
 import ca.sfu.minerva.R
-import ca.sfu.minerva.ui.map.MapFragment
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
@@ -133,7 +133,7 @@ class TrackingService: Service(), LocationListener {
             _initialAltitude = location.altitude
 
         _elapsedTime = (System.currentTimeMillis() - _mapStartMilliSec)/ 3600000F
-        _speed = (location.speed + location.speedAccuracyMetersPerSecond) * 3.6F
+        _speed = Math.max((location.speed + location.speedAccuracyMetersPerSecond) * 3.6F, _speed)
         _currentAltitude = (_initialAltitude - location.altitude)*0.001
         _lastActiveLatLng = LatLng(location.latitude, location.longitude)
         _lastLocationArray.add(location)
@@ -148,7 +148,7 @@ class TrackingService: Service(), LocationListener {
         val temp = this
 
         CoroutineScope(IO).launch {
-            val intent: Intent = Intent(temp, MapFragment::class.java)
+            val intent: Intent = Intent(temp, CoreActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
             intent.putExtra("activityFromActiveService", true)
 
@@ -207,6 +207,7 @@ class TrackingService: Service(), LocationListener {
     }
 
     override fun onDestroy() {
+        unregisterReceiver(customBroadcastReceiver)
         super.onDestroy()
         serviceTearDown()
     }
@@ -247,241 +248,10 @@ class TrackingService: Service(), LocationListener {
 
     inner class CustomBrodcastReceiver: BroadcastReceiver(){
         override fun onReceive(context: Context?, intent: Intent?) {
-            notificationManager.cancel(NOTIFICATION_ID)
             unregisterReceiver(customBroadcastReceiver)
+            notificationManager.cancel(NOTIFICATION_ID)
             serviceTearDown()
         }
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//class TrackingService: Service(), LocationListener {
-//    private lateinit var locationManager: LocationManager
-//    private lateinit var startLocation: Location
-//    private lateinit var notificationManager: NotificationManager
-//
-//    private lateinit var trackingServiceBinder: TrackingServiceBinder
-//    private var mapDataHandler: Handler? = null
-//
-//    private lateinit var locationList: ArrayList<LatLng>
-//    private var distance = 0F
-//    private var speed = 0F
-//    private var startTime = 0L
-//    private var altitude = 0.0
-//    private var totalTime = 0.0
-//
-//    companion object {
-//        const val TRACKING_MESSAGE_ID = 0
-//
-//        const val SPEED = "SPEED"
-//        const val DISTANCE = "DISTANCE"
-//        const val TOTAL_TIME = "TOTAL_TIME"
-//        const val ALTITUDE = "ALTITUDE"
-//        const val LOCATIONS = "LOCATIONS"
-//    }
-//
-//    override fun onCreate() {
-//        super.onCreate()
-//
-//        notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-//        showNotification()
-//
-//        locationList = ArrayList()
-//        startTime = System.currentTimeMillis()
-//
-//        trackingServiceBinder = TrackingServiceBinder()
-//        initLocationManager()
-//    }
-//
-//    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-//        return START_NOT_STICKY
-//    }
-//
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        cleanupTasks()
-//        stopSelf()
-//    }
-//
-//    private fun showNotification(){
-//        val temp = this
-//
-//        CoroutineScope(IO).launch {
-//            val intent: Intent = Intent(temp, MapFragment::class.java)
-//            intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
-//            intent.putExtra("activityFromActiveService", true)
-//
-//            val resultPendingIntent: PendingIntent? = TaskStackBuilder.create(temp).run {
-//                addNextIntentWithParentStack(intent)
-//                getPendingIntent(REQUEST_CODE, PendingIntent.FLAG_UPDATE_CURRENT and PendingIntent.FLAG_IMMUTABLE)
-//            }
-//
-//            val notificationBuilder : NotificationCompat.Builder = NotificationCompat.Builder(
-//                temp, CHANNEL_ID
-//            )
-//            notificationBuilder.setContentTitle("MyRuns4")
-//            notificationBuilder.setContentText("Recording your path now")
-//            notificationBuilder.setSmallIcon(R.drawable.logo)
-//            notificationBuilder.setContentIntent(resultPendingIntent)
-//            notificationBuilder.setOngoing(true)
-//            val notification = notificationBuilder.build()
-//
-//
-//            if(Build.VERSION.SDK_INT > 26){
-//                val channel = NotificationChannel(CHANNEL_ID, "myruns channel", NotificationManager.IMPORTANCE_HIGH)
-//                notificationManager.createNotificationChannel(channel)
-//            }
-//
-//            notificationManager.notify(NOTIFICATION_ID, notification)
-//            startForeground(NOTIFICATION_ID, notification)
-//        }
-//    }
-//
-//    private fun cleanupTasks() {
-//        mapDataHandler = null
-//        locationManager.removeUpdates(this)
-//        locationList.clear()
-//    }
-//
-//    override fun onBind(intent: Intent?): IBinder? {
-//        return trackingServiceBinder
-//    }
-//
-//    inner class TrackingServiceBinder: Binder() {
-//        fun setMapDataHandler(msgHandler: Handler) {
-//            this@TrackingService.mapDataHandler = msgHandler
-//        }
-//    }
-//
-//    override fun onLocationChanged(location: Location) {
-//        println("DEBUG: Location Changed")
-//        // add locations
-//        CoroutineScope(IO).launch {
-//            val lat = location.latitude
-//            val lng = location.longitude
-//            val latLng = LatLng(lat, lng)
-//            locationList.add(latLng)
-//
-//            if (::startLocation.isInitialized) {
-//                distance += Helper.metersToMiles(startLocation.distanceTo(location))
-//                speed = location.speed
-//                altitude = Helper.metersToMiles(location.altitude.toFloat()).toDouble()
-//            } else {
-//                // initialize the start location
-//                startLocation = location
-//            }
-//
-//            // send data to handler
-//            sendMapData()
-//        }
-//    }
-//
-//    private fun sendMapData() {
-//        println("DEBUG: Send Map Data")
-//        try {
-//            if (mapDataHandler != null) {
-//                val bundle = Bundle()
-//                bundle.putFloat(SPEED, speed)
-//                bundle.putFloat(DISTANCE, distance)
-//                bundle.putDouble(TOTAL_TIME, totalTime)
-//                bundle.putDouble(ALTITUDE, altitude)
-//                bundle.putString(LOCATIONS, Helper.latLngToString(locationList))
-//
-//                val mapMessage = mapDataHandler!!.obtainMessage()
-//                mapMessage.data = bundle
-//                mapMessage.what = TRACKING_MESSAGE_ID
-//                mapDataHandler!!.sendMessage(mapMessage)
-//            }
-//        } catch (t: Throwable) {
-//            Log.d("DEBUG: ", t.toString())
-//        }
-//    }
-//
-//    @SuppressLint("MissingPermission")
-//    private fun initLocationManager() {
-//        try {
-//            locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
-//            val criteria = Criteria()
-//            criteria.accuracy = Criteria.ACCURACY_FINE
-//            val provider : String? = locationManager.getBestProvider(criteria, true)
-//            if (provider != null) {
-//                val location = locationManager.getLastKnownLocation(provider)
-//                if (location != null)
-//                    onLocationChanged(location)
-//                locationManager.requestLocationUpdates(provider, 0, 0f, this)
-//            }
-//        } catch (e: SecurityException) {
-//            println("DEBUG: $e")
-//        }
-//    }
-//
-//    // this function will continuously calculate the time
-//    private suspend fun calculateTime() {
-//        TODO("Not yet implemented")
-//    }
-//}
